9612fcf66db0d8f2b470e7521982b63b3c7266e5
Iamneo <techadmin@iamneo.ai>
2023-08-07 06:38:05 +0000

    Backup Commit

--- reactapp/src/components/UI/Button/Button.js
+++ reactapp/src/components/UI/Button/Button.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Button = (props) => {
     const {
-        btnName = props.children,onClick,disabled
+        btnName = props.children,onClick,disabled,className
     } = props;
     return (
-    <button onClick={onClick} disabled={disabled}>{btnName}</button>
+    <button onClick={onClick} disabled={disabled} className={className}>{btnName}</button>
     )
   };
   
